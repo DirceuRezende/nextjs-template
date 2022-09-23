4889e9e9bb14625f902e8abb74a6909d035290a5
Dirceu Rezende <dirceu.srezende@gmail.com>
2022-09-23 14:47:17 -0300

    chore: update dependencies

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
-const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = withPWA({
-  swcMinify: true,
-  experimental: {
-    // Enables the styled-components SWC transform
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
     styledComponents: true
-  },
-  pwa: {
-    dest: 'public',
-    disable: !isProd
   }
-})
+}
+
+module.exports = nextConfig
